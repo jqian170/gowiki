f3548bed5e4d20ca32747c40ac3996769f00bbdb
Jie Qian <qian@Jies-MacBook-Pro.local>
2020-09-01 00:34:16 -0700

    w/o literals and closure

--- wiki.go
+++ wiki.go
@@ -3,16 +3,18 @@
 package main
 
 import (
-  "fmt"
   "io/ioutil"
   "log"
   "net/http"
+  "html/template"
 )
 
 type Page struct {
   Title string
   Body []byte
 }
+
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
 
 func (p *Page) save() error {
   filename := p.Title + ".txt"
@@ -28,21 +30,58 @@ func loadPage(title string) (*Page, error) {
   return &Page{Title: title, Body: body}, nil
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Handler test, %s", r.URL.Path[1:])
+// func handler(w http.ResponseWriter, r *http.Request) {
+//   fmt.Fprintf(w, "Handler test, %s", r.URL.Path[1:])
+// }
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+  title := r.URL.Path[len("/view/"):]
+  p, err := loadPage(title)
+  if err != nil {
+    http.Redirect(w, r, "/edit/" + title, http.StatusFound)
+    return
+  }
+  renderTemplate(w, "view", p)
 }
 
-func viewhandler(w http.ResponseWriter, r *http.Request) {
-  title := r.URL.Path[len("/view/"):]
-  p, _ := loadPage(title)
-  fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+func editHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/edit/"):]
+    p, err := loadPage(title)
+    if err != nil {
+        p = &Page{Title: title}
+    }
+    renderTemplate(w, "edit", p)
 }
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/save/"):]
+    body := r.FormValue("body")
+    p := &Page{Title: title, Body: []byte(body)}
+    err := p.save()
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    http.Redirect(w, r, "/view/" + title, http.StatusFound)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+    err := templates.ExecuteTemplate(w, tmpl + ".html", p)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
+
 func main() {
-  // p1 := &Page{Title: "Test", Body: []byte("This is test")}
-  // p1.save()
-  // p2, _ := loadPage("Test")
-  // fmt.Println(string(p2.Body))
-  http.HandleFunc("/", handler)
-  http.HandleFunc("/view/", viewhandler)
+  /*
+  p1 := &Page{Title: "Test", Body: []byte("This is test")}
+  p1.save()
+  p2, _ := loadPage("Test")
+  fmt.Println(string(p2.Body))
+  */
+  // http.HandleFunc("/", handler)
+  http.HandleFunc("/view/", viewHandler)
+  http.HandleFunc("/edit/", editHandler)
+  http.HandleFunc("/save/", saveHandler)
   log.Fatal(http.ListenAndServe(":8080", nil))
 }
